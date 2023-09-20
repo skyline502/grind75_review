0e54ef276c03289907ae4b4604ce8e1996868512
Johnny San <johnny.san@gmail.com>
2023-09-20 09:14:16 -0700

    added merge two sorted lists

--- week1/03-merge-two-sorted-lists.js
+++ week1/03-merge-two-sorted-lists.js
@@ -6,3 +6,32 @@
 // Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 // Return the head of the merged linked list.
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+const mergeTwoSortedLists = (head1, head2) => {
+  console.log(head1, head2)
+}
+
+//examples
+
+const a = new Node(1)
+const b = new Node(2)
+const c = new Node(4)
+
+a.next = b
+b.next = c
+
+const d = new Node(1)
+const e = new Node(3)
+const f = new Node(4)
+
+d.next = e
+e.next = f
+
+console.log(mergeTwoSortedLists(a, d))//=>[1, 1, 2, 3, 4, 4]
