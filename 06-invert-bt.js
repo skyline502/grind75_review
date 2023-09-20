2b1fcaf39473a5cfc5f605eb7b22bc32993ab8e9
Johnny San <johnny.san@gmail.com>
2023-09-20 10:13:43 -0700

    added inverted binary tree

---
+++ 06-invert-bt.js
@@ -0,0 +1,55 @@
+// 226. Invert Binary Tree
+// Easy
+
+// Given the root of a binary tree, invert the tree, and return its root.
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+
+const invertTree = root => {
+
+}
+
+// Example 1:
+
+
+// Input: root = [4,2,7,1,3,6,9]
+
+const a = new Node(4)
+const b = new Node(2)
+const c = new Node(7)
+const d = new Node(1)
+const e = new Node(3)
+const f = new Node(6)
+const g = new Node(9)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+c.right = g
+
+// Output: [4,7,2,9,6,3,1]
+
+
+// Example 2:
+
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
