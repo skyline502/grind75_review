a8b92f3a0969d2f526e575d44b7159c6f9d811f6
Johnny San <johnny.san@gmail.com>
2023-09-22 11:56:04 -0700

    solved valid anagram

--- week1/07-valid-anagram.js
+++ week1/07-valid-anagram.js
@@ -6,18 +6,42 @@
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
 const validAnagram = (s1,s2) => {
+  if (s1.length !== s2.length) return false;
+  let count = {}
 
+  for (let char of s1) {
+    if (!count[char]) {
+      count[char] = 1;
+    } else {
+      count[char]++;
+    }
+  }
+
+  console.log(count, 'count')
+
+  for (let char of s2) {
+    if (!char in count){
+      return false;
+    } else {
+      count[char]--;
+    }
+  }
+
+  for (let char in count) {
+    if (count[char] !== 0) return false;
+  }
+  return true;
 }
 
 
 // Example 1:
 let s = "anagram", t = "nagaram"
-console.log(validAnagram(s))
+console.log(validAnagram(s,t))
 // Output: true
 
 // Example 2:
 s = "rat", t = "car"
-console.log(validAnagram(s))
+console.log(validAnagram(s,t))
 // Output: false
 
 
