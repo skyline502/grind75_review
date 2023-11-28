2d62594e230eb9e7a6ac83978dd9ff582f6c6d9b
Johnny San <johnny.san@gmail.com>
2023-11-28 10:09:22 -0800

    added helper function

--- week1/09-flood-fill.js
+++ week1/09-flood-fill.js
@@ -6,7 +6,8 @@ An image is represented by an m x n integer grid image where image[i][j] represe
 
 You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
 
-To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels
+connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
 
 Return the modified image after performing the flood fill.
 
@@ -18,7 +19,8 @@ Example 1:
 
 Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
 Output: [[2,2,2],[2,2,0],[2,0,1]]
-Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e.,
+the blue pixels) are colored with the new color.
 Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
 Example 2:
 
@@ -38,6 +40,15 @@ n == image[i].length
 */
 
 const floodFill = (img, sr, sc, color) => {
+    let start = [sr, sc];
+    let neighbors = [];
+
+}
+
+const findNeighbors = (img, sr, sc, color, neighbors) => {
+  const rowValid = 0 >= sr <= img.length - 1;
+  const colValid = 0 >= sc <= img[0].length - 1;
+
 
 }
 
